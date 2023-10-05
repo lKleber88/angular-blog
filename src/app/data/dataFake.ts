4dbd71f065879dd75f97ad3914f4367cb6f9e2d4
Luiz Kleber <kalebst330@gmail.com>
2023-10-05 10:08:33 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Kyojuro Rengoku",
+    "description": "Hashira das Chamas",
+    "photoCover": "https://i.pinimg.com/originals/53/52/b6/5352b6f622b7733364c2702cfd27652e.gif"
+  },
+  {
+    "id":"2",
+    "title": "Giyu Tomioka",
+    "description": "Hashira da Água",
+    "photoCover": "https://media.tenor.com/3aeaQBgMdWwAAAAd/giyu-tomioka-giyu.gif"
+  },
+  {
+    "id":"3",
+    "title": "Mitsuri Kanroji",
+    "description": "Hashira do Amor",
+    "photoCover": "https://static.wikia.nocookie.net/kimetsu-no-yaiba/images/e/e3/Mitsuri_using_Swaying_Love_Wildclaw.gif"
+  },
+  {
+    "id":"4",
+    "title": "Tengen Uzui",
+    "description": "Hashira do Som",
+    "photoCover": "https://static.wikia.nocookie.net/kimetsu-no-yaiba/images/b/ba/Constant_Resounding_Slashes_vs_Rotating_Circular_Slashes_-_Flying_Blood_Sickles.gif"
+  }
+]
